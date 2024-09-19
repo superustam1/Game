c179f679cbdc93efa7518633797bbc10d82f903a
superustam1 <superustam1@gmail.com>
2024-09-19 15:24:35 +0200

    Hello

--- 2dgame-main/player.py
+++ 2dgame-main/player.py
@@ -12,7 +12,7 @@ BLACK = (0, 0, 0)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Spritesheets')
-Keys = {1:"One"}
+
 
 class player:
     def __init__(self):
@@ -28,8 +28,17 @@ class player:
         self.walk_down_animation = self.createanimation('Knight_10_Walk_Down.png', 4)
         self.walk_left_animation = self.createanimation('Knight_10_Walk_Left.png', 4)
         self.walk_right_animation = self.createanimation('Knight_10_Walk_Right.png', 4)
+        self.sword_up_animation = self.createanimation('Knight_10_Sword_Up.png', 4)
+        self.sword_down_animation = self.createanimation('Knight_10_Sword_Down.png', 4)
+        self.sword_left_animation = self.createanimation('Knight_10_Sword_Left.png', 4)
+        self.sword_right_animation = self.createanimation('Knight_10_Sword_Right.png', 4)
         self.current_animation = self.walk_down_animation
-        self.Idle = True
+        self.idle = True
+        self.keys = {pygame.K_a: [-1, self.walk_left_animation],
+                     pygame.K_d: [1, self.walk_right_animation],
+                     pygame.K_w: [-1, self.walk_up_animation],
+                     pygame.K_s: [1, self.walk_down_animation]}
+        self.last_key_pressed = pygame.K_s
 
 
 
@@ -41,38 +50,28 @@ class player:
             self.frame_list.append(self.sprite_sheet.get_image(x, 32, 32, 3, BLACK))
         return self.frame_list
 
-    def detectkey(self,event):
+    def detectinput(self,event):
+#        if event.type == pygame.MOUSEBUTTONDOWN:
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a or event.key == pygame.K_d or event.key == pygame.K_w or event.key == pygame.K_s:
-                self.Idle = False
-            if event.key == pygame.K_a:
-                self.player_x_direction = -1
-                self.current_animation = self.walk_left_animation
-            if event.key == pygame.K_d:
-                self.player_x_direction = 1
-                self.current_animation = self.walk_right_animation
-            if event.key == pygame.K_w:
-                self.player_y_direction = -1
-                self.current_animation = self.walk_up_animation
-            if event.key == pygame.K_s:
-                self.player_y_direction = 1
-                self.current_animation = self.walk_down_animation
-
-
+            if event.key in self.keys:
+                self.idle = False
+                self.current_animation = self.keys[event.key][1]
+                if event.key == pygame.K_a or event.key == pygame.K_d:
+                    self.player_x_direction = self.keys[event.key][0]
+                elif event.key == pygame.K_w or event.key == pygame.K_s:
+                    self.player_y_direction = self.keys[event.key][0]
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_a or event.key == pygame.K_d or event.key == pygame.K_w or event.key == pygame.K_s:
-                self.Idle = True
-            if event.key == pygame.K_a:
-                self.player_x_direction = 0
-            if event.key == pygame.K_d:
-                self.player_x_direction = 0
-            if event.key == pygame.K_w:
-                self.player_y_direction = 0
-            if event.key == pygame.K_s:
-                self.player_y_direction = 0
+            if event.key in self.keys:
+                self.idle = True
+                self.current_animation = self.keys[event.key][1]
+                if event.key == pygame.K_a or event.key == pygame.K_d:
+                    self.player_x_direction = 0
+                elif event.key == pygame.K_w or event.key == pygame.K_s:
+                    self.player_y_direction = 0
 
 
-    def update_player_position(self):
+
+    def update_player_position(self,):
         if self.player_x_direction > 0:
             if self.player_x <500 - self.player_width:
                 self.player_x += self.player_x_direction * self.player_speed
@@ -103,12 +102,12 @@ while run:
         last_update = current_time
         if frame >= len(Player.current_animation):
             frame = 0
-        if Player.Idle == True:
+        if Player.idle == True:
             frame = 0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        Player.detectkey(event)
+        Player.detectinput(event)
 
     screen.blit(Player.current_animation[frame], (Player.player_x-Player.player_width/2, Player.player_y-Player.player_height/2))
     Player.update_player_position()

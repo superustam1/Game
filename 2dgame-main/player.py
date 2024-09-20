005cd4554964dd6d72a04706fb8019c60e54b7ba
superustam1 <superustam1@gmail.com>
2024-09-20 17:26:04 +0200

    Hello

--- 2dgame-main/player.py
+++ 2dgame-main/player.py
@@ -16,16 +16,10 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Spritesheets')
 
 
-class player:
-    def __init__(self):
-        self.player_health = 3
-        self.player_width = 50
-        self.player_height = 50
-        self.player_x = 250
-        self.player_y = 250
-        self.player_speed = 0.1
-        self.player_x_direction = 0
-        self.player_y_direction = 0
+class Player:
+    def __init__(self, Screen):
+        self.screen = Screen
+
         self.walk_up_animation = self.create_animation('Knight_10_Walk_Up.png',4)
         self.walk_down_animation = self.create_animation('Knight_10_Walk_Down.png', 4)
         self.walk_left_animation = self.create_animation('Knight_10_Walk_Left.png', 4)
@@ -34,23 +28,32 @@ class player:
         self.sword_down_animation = self.create_animation('Knight_10_Sword_Down.png', 4)
         self.sword_left_animation = self.create_animation('Knight_10_Sword_Left.png', 4)
         self.sword_right_animation = self.create_animation('Knight_10_Sword_Right.png', 4)
+
         self.current_animation = self.walk_down_animation
         self.idle = True
+        self.frame = 0
+        self.animation_cooldown = 200
+        self.sword = False
+        self.sword_cooldown = False
+        self.sword_cooldown_time = 1000
+        self.last_update = pygame.time.get_ticks()
+        self.last_sword_update = pygame.time.get_ticks()
+
+        self.player_health = 3
+        self.player_width = 50
+        self.player_height = 50
+        self.player_x = 250
+        self.player_y = 250
+        self.player_speed = 0.1
+        self.player_x_direction = 0
+        self.player_y_direction = 0
+
         self.keys = {pygame.K_a: [-1, self.walk_left_animation,self.sword_left_animation],
                      pygame.K_d: [1, self.walk_right_animation,self.sword_right_animation],
                      pygame.K_w: [-1, self.walk_up_animation,self.sword_up_animation],
                      pygame.K_s: [1, self.walk_down_animation,self.sword_down_animation]}
         self.currently_pressed_keys = []
-        self.last_update = pygame.time.get_ticks()
-        self.last_sword_update = pygame.time.get_ticks()
-        self.frame = 0
         self.last_key_pressed = pygame.K_s
-        self.animation_cooldown = 200
-        self.sword = False
-        self.sword_cooldown = False
-        self.sword_cooldown_time = 1000
-
-
 
     def switch_animation(self,new_animation=None):
         if self.sword == False:
@@ -71,6 +74,7 @@ class player:
             # This if is for interacting with things
             if event.button == 2:
                 pass
+
         if event.type == pygame.KEYDOWN:
             if event.key in self.keys:
                 self.last_key_pressed = event.key
@@ -106,7 +110,7 @@ class player:
             self.frame = 0
         if self.current_time - self.last_sword_update >= self.sword_cooldown_time:
             self.sword_cooldown = False
-        screen.blit(self.current_animation[self.frame],(self.player_x - self.player_width / 2, self.player_y - self.player_height / 2))
+        self.screen.blit(self.current_animation[self.frame],(self.player_x - self.player_width / 2, self.player_y - self.player_height / 2))
 
 
 
@@ -134,7 +138,7 @@ class player:
         return self.frame_list
 
 
-Player = player()
+player = Player(screen)
 
 run = True
 while run:
@@ -142,9 +146,9 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        Player.detect_input(event)
-    Player.play_animation()
-    Player.update_player_position()
+        player.detect_input(event)
+    player.play_animation()
+    player.update_player_position()
     #    pygame.draw.rect(screen,BLACK,[Player.player_x,Player.player_y,Player.player_width,Player.player_height])
     pygame.display.update()
 pygame.quit()

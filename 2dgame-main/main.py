c1454c963d3f28f8bfc16e1b195223db0bb2a65d
superustam1 <superustam1@gmail.com>
2024-11-12 08:35:23 +0100

    Hello

--- 2dgame-main/main.py
+++ 2dgame-main/main.py
@@ -1,48 +1,166 @@
-#основной для запуска
-import pygame 
-import sys
-from menu import show_menu
+import pygame
+from pytmx.util_pygame import load_pygame
 from player import Player
-from map import Map
-from npcs import Monster
 
-#Инициализация pygame
+
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 640
+BG = (50, 50, 50)
+
 pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+tmx_data = load_pygame('./Maps/FirstMap.tmx')
+sprite_group = pygame.sprite.Group()
 
-#настройки окна
-screen_wigth = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_wigth, screen_height))
-pygame.display.set_caption('Dangeon Shooter-Platformer-Visual Novel')
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, sprites):
+        super().__init__(sprites)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.copy()
 
-#Создание обьектов
-player = Player(50,50)
-map = Map('png')
-monster = Monster(300,200)
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, sprites,rects, height=None, width=None):
+        super().__init__(sprites)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.copy()
+        self.center = self.hitbox.center
+        rects.append(self.hitbox)
+        if height:
+            self.height = height
+            self.hitbox.width -= 20
+            pos[1] += 64
+            self.hitbox.bottomleft = pos
+            self.hitbox.height = self.height
+            self.hitbox.centerx = self.center[0]
+        if width:
+            self.width = width
 
-#основной цикл игры
-def game_loop():
-    running =True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        player.update()
-
-        screen.fill((0,0,0))
-        map.draw(screen)
-        player.draw(screen)
-        monster.draw(screen)
-        pygame.displey.flip()
-
-if __name__ == '__main__':
-    show_menu(screen)
-    game_loop()
-
+            self.hitbox.width = self.width
+            self.hitbox.center = self.center
 
 
 
 
+def CreateSpriteGroup(Layer_Name):
+    sprite_group = pygame.sprite.Group()
 
+    for layer in tmx_data.visible_layers:
+        if hasattr(layer, 'data') and layer.name == Layer_Name:
+            tile_dict = {(x, y): surf for x, y, surf in tmx_data.get_layer_by_name(Layer_Name).tiles()}
+            processed_tiles = set()  # To keep track of processed tiles
+
+            for x, y, surf in layer.tiles():
+                if (x, y) not in processed_tiles:
+                    pos = [x * tmx_data.tilewidth, y * tmx_data.tileheight]
+
+                    tree_image = pygame.Surface((tmx_data.tilewidth * 2, tmx_data.tileheight * 2), pygame.SRCALPHA)
+                    tree_image.blit(surf, (0, 0))  # Top-left
+                    tree_image.blit(tile_dict.get((x + 1, y)), (tmx_data.tilewidth, 0))  # Top-right
+                    tree_image.blit(tile_dict.get((x, y + 1)), (0, tmx_data.tileheight))  # Bottom-left
+                    tree_image.blit(tile_dict.get((x + 1, y + 1)),
+                    (tmx_data.tilewidth, tmx_data.tileheight))  # Bottom-right
+                    tile = Tile(pos, tree_image,sprite_group)
+
+                    processed_tiles.update([(x, y), (x + 1, y), (x, y + 1), (x + 1, y + 1)])
+    return sprite_group
+
+
+def load_walls_from_tmx(layer_name):
+    wall_group = pygame.sprite.Group()
+    wall_rects = []
+    layer = tmx_data.get_layer_by_name(layer_name)
+    tile_width = tmx_data.tilewidth
+    tile_height = tmx_data.tileheight
+
+    # Dictionary of all tiles on the specified layer
+    tile_dict = {(x, y): surf for x, y, surf in layer.tiles()}
+    processed_tiles = set()  # To keep track of processed tiles
+
+    for (x, y), surf in tile_dict.items():
+        if (x, y) in processed_tiles:
+            continue  # Skip already processed tiles
+
+        # Check for horizontal wall segment
+        wall_length = 1
+        while (x + wall_length, y) in tile_dict and (x + wall_length, y) not in processed_tiles:
+            wall_length += 1
+
+        if wall_length > 1:  # Horizontal wall detected
+            pos = [x * tile_width, y * tile_height]
+            wall_image = pygame.Surface((wall_length * tile_width, tile_height), pygame.SRCALPHA)
+
+            # Blit each tile in the wall segment onto the wall_image
+            for i in range(wall_length):
+                wall_image.blit(tile_dict[(x + i, y)], (i * tile_width, 0))
+                processed_tiles.add((x + i, y))
+
+            # Create and add the horizontal wall sprite as one contiguous sprite
+            wall_sprite = Wall(pos, wall_image, wall_group,wall_rects,height=5)
+
+        else:  # Check for vertical wall segment if no horizontal wall is found
+            wall_height = 1
+            while (x, y + wall_height) in tile_dict and (x, y + wall_height) not in processed_tiles:
+                wall_height += 1
+
+            if wall_height > 1:  # Vertical wall detected
+                pos = [x * tile_width, y * tile_height]
+                wall_image = pygame.Surface((tile_width, (wall_height) * tile_height), pygame.SRCALPHA)
+
+                # Blit each tile in the vertical wall segment onto the wall_image
+                for j in range(wall_height-1):
+                    wall_image.blit(tile_dict[(x, y + j)], (0, j * tile_height))
+                    processed_tiles.add((x, y + j))
+
+                # Create and add the vertical wall sprite as one contiguous sprite
+                wall_sprite = Wall(pos, wall_image, wall_group,wall_rects,width=12)
+
+    return wall_group, wall_rects
+
+
+def CreateLayerImage(layerNames):
+    layer_surface = pygame.Surface((960, 640),pygame.SRCALPHA)
+    for layer in tmx_data.visible_layers:
+        if hasattr(layer,'data') and layer.name in layerNames:
+            for x,y,surf in layer.tiles():
+                pos = (x * 32, y * 32)
+                layer_surface.blit(surf, pos)
+    return layer_surface
+
+
+
+all_sprites = pygame.sprite.LayeredUpdates()
+
+player = Player(screen,SCREEN_WIDTH,SCREEN_HEIGHT)
+
+Ground_Layer = CreateLayerImage(("Ground","Path"))
+Apples_Layer = CreateLayerImage("Apples")
+Walls_Sprites, Walls_Rects = load_walls_from_tmx("Walls")
+Tree_group = CreateSpriteGroup("Trees")
+
+all_sprites.add(player)
+all_sprites.add(Tree_group)
+all_sprites.add(Walls_Sprites)
+run = True
+Num = 0
+
+while run:
+    print(Num)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        player.detect_input(event)
+    screen.fill(BG)
+    screen.blit(Ground_Layer, (0, 0))
+    screen.blit(Apples_Layer, (0, 0))
+    player.update(Walls_Rects)
+    print(player.current_time)
+
+    for sprite in sorted(all_sprites, key=lambda sprite: sprite.hitbox.bottom):
+        screen.blit(sprite.image, sprite.rect)
+        #pygame.draw.rect(screen,(255, 0, 0),sprite.hitbox,1)
+    pygame.display.update()
+    Num += 1
+
+pygame.quit()
--- 2dgame-main/player.py
+++ 2dgame-main/player.py
@@ -1,36 +1,10 @@
 import pygame
 import spritesheet
-from pytmx.util_pygame import load_pygame
-
-SCREEN_WIDTH = 960
-SCREEN_HEIGHT = 640
-BG = (50, 50, 50)
-BLACK = (0, 0, 0)
-
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-tmx_data = load_pygame('./Maps/FirstMap.tmx')
-sprite_group = pygame.sprite.Group()
-
-# class Tile(pygame.sprite.Sprite):
-#     def __init__(self, pos, surf, groups):
-#         super().__init__(groups)
-#         self.image = surf
-#         self.rect = self.image.get_rect(topleft = pos)
-
-def CreateLayerImage(layerName):
-    layer_surface = pygame.Surface((960, 640),pygame.SRCALPHA)
-    for layer in tmx_data.visible_layers:
-        if hasattr(layer,'data') and layer.name == layerName:
-            for x,y,surf in layer.tiles():
-                pos = (x * 32, y * 32)
-                layer_surface.blit(surf, pos)
-    return layer_surface
 
 
-
-class Player:
+class Player(pygame.sprite.Sprite):
     def __init__(self, Screen,ScreenWidth,ScreenHeight):
+        super().__init__()
         self.screen = Screen
         self.screenwidth = ScreenWidth
         self.screenheight = ScreenHeight
@@ -49,21 +23,21 @@ class Player:
         self.frame = 0
         self.animation_cooldown = 200
 
-        self.sword = False
+        self.using_sword = False
         self.sword_cooldown = False
         self.sword_cooldown_time = 1000
-        self.last_update = pygame.time.get_ticks()
-        self.last_sword_update = pygame.time.get_ticks()
+        self.last_frame_update = pygame.time.get_ticks()
+        self.last_sword_use = pygame.time.get_ticks()
 
         self.player_health = 3
-        self.player_width = 50
-        self.player_height = 50
-        self.player_x = 250
-        self.player_y = 250
+        self.rect = self.current_animation[0].get_rect()
+        self.image = self.current_animation[0]
         self.player_speed = 1.5
+        self.vector_correction = 1
+        self.player_x = 200
+        self.player_y = 200
         self.player_x_direction = 0
         self.player_y_direction = 0
-        self.vector_correction = 1
         self.position_change_cooldown = 1
         self.last_position_update = pygame.time.get_ticks()
 
@@ -77,31 +51,31 @@ class Player:
         self.xkeys_pressed = ["None"]
         self.ykeys_pressed = ["None"]
 
-    def update(self):
-        self.current_time = pygame.time.get_ticks()
-        self.play_animation()
-        self.update_player_position()
+        self.padding_x = 23
+        self.padding_y = 17
+        self.hitbox = pygame.Rect(self.rect.x + self.padding_x,
+                                  self.rect.y + self.padding_y, 50, 62)
 
-    def switch_animation(self,Index=1):
-        if self.sword:
-            Index = 2
-        if self.currently_pressed_keys:
-            self.current_animation = self.keys[self.currently_pressed_keys[-1]][Index]
-        else:
-            self.current_animation = self.keys[self.last_key_pressed][Index]
+        self.next_x = self.player_x + self.player_x_direction * self.player_speed * self.vector_correction
+        self.next_y = self.player_y + self.player_y_direction * self.player_speed * self.vector_correction
+        self.next_hitbox_x = self.hitbox.move(self.next_x - self.hitbox.x, 0)
+        self.next_hitbox = self.hitbox.move(0, self.next_y - self.hitbox.y)
+
+        self.left_side = pygame.Rect(self.hitbox.left - 1, self.hitbox.bottom, 1, 4)
+        self.right_side = pygame.Rect(self.hitbox.right, self.hitbox.bottom, 1, 4)
+        self.top_side = pygame.Rect(self.hitbox.left, self.hitbox.bottom - 1, 46, 1)
+        self.bottom_side = pygame.Rect(self.hitbox.left, self.hitbox.bottom, 46, 1)
 
 
-    def changedirection(self,):
-        if pygame.K_a in self.currently_pressed_keys or pygame.K_d in self.currently_pressed_keys or "None":
-            self.player_x_direction = self.keys[self.xkeys_pressed[-1]][0]
-        if pygame.K_w in self.currently_pressed_keys or pygame.K_s in self.currently_pressed_keys or "None":
-            self.player_y_direction = self.keys[self.ykeys_pressed[-1]][0]
-        self.switch_animation()
 
-        if self.player_x_direction + self.player_y_direction in (-1, 1):
-            self.vector_correction = 1
-        else:
-            self.vector_correction = 0.7071
+    def create_animation(self,sprite_sheet_image,animation_steps):
+        self.sprite_sheet_image = pygame.image.load(sprite_sheet_image).convert_alpha()
+        self.sprite_sheet = spritesheet.SpriteSheet(self.sprite_sheet_image)
+        self.frame_list = []
+        for x in range(animation_steps):
+            self.frame_list.append(self.sprite_sheet.get_image(x, 32, 32, 3, (0,0,0)))
+        return self.frame_list
+
 
     def detect_input(self,event):
         if event.type == pygame.KEYDOWN:
@@ -110,7 +84,7 @@ class Player:
                 self.idle = False
                 if event.key in (pygame.K_a, pygame.K_d):
                     self.xkeys_pressed.append(event.key)
-                else:
+                elif event.key in (pygame.K_w, pygame.K_s):
                     self.ykeys_pressed.append(event.key)
 
                 self.changedirection()
@@ -130,79 +104,121 @@ class Player:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1 and self.sword_cooldown == False:
-                self.sword = True
+                self.using_sword = True
                 self.animation_cooldown = 50
                 self.switch_animation()
                 self.frame = 0
                 self.player_speed = 5
                 self.sword_cooldown = True
-                self.last_sword_update = self.current_time
+                self.last_sword_use = self.current_time
             # This if is for interacting with things
             if event.button == 2:
                 pass
 
+    def update(self,walls_group):
+        self.current_time = pygame.time.get_ticks()
+        self.play_animation()
+        self.update_player_position(walls_group)
+
     def play_animation(self,):
-        if self.current_time - self.last_update >= self.animation_cooldown:
+        if self.current_time - self.last_frame_update >= self.animation_cooldown:
             self.frame += 1
-            self.last_update = self.current_time
+            self.last_frame_update = self.current_time
         if self.frame >= len(self.current_animation):
             self.frame = 0
-            self.sword = False
+            self.using_sword = False
             self.animation_cooldown = 200
             self.player_speed = 1.5
             self.switch_animation()
-        if self.idle and self.sword == False:
+        if self.idle and self.using_sword == False:
             self.frame = 0
-        if self.current_time - self.last_sword_update >= self.sword_cooldown_time:
+        if self.current_time - self.last_sword_use >= self.sword_cooldown_time:
             self.sword_cooldown = False
-        self.screen.blit(self.current_animation[self.frame],(self.player_x - 23, self.player_y - 22))
+        self.image = self.current_animation[self.frame]
+        #pygame.draw.rect(self.screen, (255, 0, 0), self.hitbox, 1)
+#        pygame.draw.rect(self.screen, (255, 0, 0), self.next_hitbox_x, 1)
+#        pygame.draw.rect(self.screen, (255, 0, 0), self.next_hitbox, 1)
+#        pygame.draw.rect(self.screen, (255, 0, 0), self.left_side, 1)
+#        pygame.draw.rect(self.screen, (255, 0, 0), self.right_side, 1)
+#        pygame.draw.rect(self.screen, (255, 0, 0), self.top_side, 1)
+#        pygame.draw.rect(self.screen, (255, 0, 0), self.bottom_side, 1)
 
-    def update_player_position(self,):
+    def update_player_position(self,walls_group):
         if self.current_time - self.last_position_update >= 10:
-            if self.player_x_direction > 0:
-                if self.player_x <self.screenwidth - self.player_width:
-                    self.player_x += self.player_x_direction * self.player_speed * self.vector_correction
-            if self.player_x_direction < 0:
-                if self.player_x > 0:
-                    self.player_x += self.player_x_direction * self.player_speed * self.vector_correction
-            if self.player_y_direction > 0:
-                if self.player_y <self.screenheight - self.player_height:
-                    self.player_y += self.player_y_direction * self.player_speed * self.vector_correction
-            if self.player_y_direction < 0:
-                if self.player_y > 0:
-                    self.player_y += self.player_y_direction * self.player_speed * self.vector_correction
+
+            self.next_x = self.player_x + self.player_x_direction * self.player_speed * self.vector_correction
+            self.next_y = self.player_y + self.player_y_direction * self.player_speed * self.vector_correction
+
+            # Create a full-sized hitbox for screen edge detection
+            next_full_hitbox = self.hitbox.move(self.next_x - self.hitbox.x + self.padding_x, self.next_y - self.hitbox.y + self.padding_y)
+
+            # Create thin hitboxes for precise wall collision on each side
+            self.next_hitbox = self.hitbox.move(2, self.next_y - self.hitbox.y + self.padding_y + self.hitbox.height - 4)
+            self.next_hitbox.height = 4
+            self.next_hitbox.width = 46
+
+            # Define smaller side rects based on player dimensions
+            self.left_side = pygame.Rect(self.next_hitbox.left - 1, self.next_hitbox.top, 1, self.next_hitbox.height)
+            self.right_side = pygame.Rect(self.next_hitbox.right, self.next_hitbox.top, 1, self.next_hitbox.height)
+            self.top_side = pygame.Rect(self.next_hitbox.left, self.next_hitbox.top - 1, self.next_hitbox.width, 1)
+            self.bottom_side = pygame.Rect(self.next_hitbox.left, self.next_hitbox.bottom, self.next_hitbox.width, 1)
+
+            # Check collisions with walls
+            self.is_touching_left = self.left_side.collidelist(walls_group) != -1
+            self.is_touching_right = self.right_side.collidelist(walls_group) != -1
+            self.is_touching_top = self.top_side.collidelist(walls_group) != -1
+            self.is_touching_bottom = self.bottom_side.collidelist(walls_group) != -1
+
+            # Screen boundaries for original hitbox size
+            screen_left = 0
+            screen_right = self.screenwidth
+            screen_top = 0
+            screen_bottom = self.screenheight
+
+            # Check horizontal movement
+            if screen_left <= next_full_hitbox.left and next_full_hitbox.right <= screen_right:
+                if self.player_x_direction > 0 and not self.is_touching_right:
+                    # Moving right and not colliding with right wall or screen edge
+                    self.player_x = self.next_x
+                elif self.player_x_direction < 0 and not self.is_touching_left:
+                    # Moving left and not colliding with left wall or screen edge
+                    self.player_x = self.next_x
+
+            # Check vertical movement
+            if screen_top <= next_full_hitbox.top and next_full_hitbox.bottom <= screen_bottom:
+                if self.player_y_direction > 0 and not self.is_touching_bottom:
+                    # Moving down and not colliding with bottom wall or screen edge
+                    self.player_y = self.next_y
+                elif self.player_y_direction < 0 and not self.is_touching_top:
+                    # Moving up and not colliding with top wall or screen edge
+                    self.player_y = self.next_y
+
+            # Update the player's rect and hitbox positions
+            self.rect.x, self.rect.y = self.player_x, self.player_y
+            self.hitbox.x = self.rect.x + self.padding_x
+            self.hitbox.y = self.rect.y + self.padding_y
             self.last_position_update = self.current_time
 
-    def create_animation(self,sprite_sheet_image,animation_steps):
-        self.sprite_sheet_image = pygame.image.load(sprite_sheet_image).convert_alpha()
-        self.sprite_sheet = spritesheet.SpriteSheet(self.sprite_sheet_image)
-        self.frame_list = []
-        for x in range(animation_steps):
-            self.frame_list.append(self.sprite_sheet.get_image(x, 32, 32, 3, BLACK))
-        return self.frame_list
-
-player = Player(screen,SCREEN_WIDTH,SCREEN_HEIGHT)
-
-Ground_Layer = CreateLayerImage("Ground")
-Path_Layer = CreateLayerImage("Path")
-Apples_Layer = CreateLayerImage("Apples")
-Walls_Layer = CreateLayerImage("Walls")
-Trees_Layer = CreateLayerImage("Trees")
-
-run = True
+    def switch_animation(self,Index=1):
+        if self.using_sword:
+            Index = 2
+        if self.currently_pressed_keys:
+            self.current_animation = self.keys[self.currently_pressed_keys[-1]][Index]
+        else:
+            self.current_animation = self.keys[self.last_key_pressed][Index]
 
 
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        player.detect_input(event)
-    screen.blit(Ground_Layer, (0, 0))
-    screen.blit(Path_Layer, (0, 0))
-    screen.blit(Apples_Layer, (0, 0))
-    player.update()
-    screen.blit(Walls_Layer, (0, 0))
-    screen.blit(Trees_Layer, (0, 0))
-#    pygame.draw.rect(screen,BLACK,[player.player_x,player.player_y,player.player_width,player.player_height])
-    pygame.display.update()
-pygame.quit()
+    def changedirection(self,):
+        if pygame.K_a in self.currently_pressed_keys or pygame.K_d in self.currently_pressed_keys or "None":
+            self.player_x_direction = self.keys[self.xkeys_pressed[-1]][0]
+        if pygame.K_w in self.currently_pressed_keys or pygame.K_s in self.currently_pressed_keys or "None":
+            self.player_y_direction = self.keys[self.ykeys_pressed[-1]][0]
+        self.switch_animation()
+
+        self.vector_correction = 1
+        if self.player_x_direction + self.player_y_direction in (-2, 0, 2):
+            self.vector_correction = 0.7071
+
+
+
+
